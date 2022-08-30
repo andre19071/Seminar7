2cc77ddb73bdf49fa2354fcba345b466ad0cb26c
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-30 12:16:20 +0700

    task48

--- task48/Program.cs
+++ task48/Program.cs
@@ -4,3 +4,36 @@
 // 0 1 2 3
 // 1 2 3 4
 // 2 3 4 5
+
+int[,] FillMatrix(int rowsCount, int columnsCount)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());//
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());//
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
