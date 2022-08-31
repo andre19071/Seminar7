bee47a4794e775c46f9a8a11915e1514d564c4fb
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-31 13:19:43 +0700

    task51

--- task51/Program.cs
+++ task51/Program.cs
@@ -6,3 +6,64 @@
 // 8 4 2 4
 // Сумма элементов главной диагонали: 1+9+2 = 12
 // Доп. условие для 51 задачи: сделать суммирование в один цикл. Матрица может быть прямоугольный
+
+// Решение в один цикл
+// int sum = 0;
+// int min = m;
+// if (n < min)
+// {
+//     min = n;
+// }
+// for (int i = 0; i < min; i++)
+// {
+//          sum += matrix[i,i] ;
+// }
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+
+int sum = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            sum += matrix[i, j];
+        }
+    }
+}
+
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine(sum);
